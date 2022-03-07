2384354e8cd5bbf365d93d1264c5a0eeb6505995
Felix <felix.ulmanu@gmail.com>
2022-03-07 14:22:06 -0500

    Also stow firefox (oops)

--- scripts/linkdots.sh
+++ scripts/linkdots.sh
@@ -11,6 +11,11 @@ cd "$DIR" || exit
 
 # symlink configs
 stow --restow --target="$XDG_CONFIG_HOME" .config
+# firefox config
+for dir in ~/.mozilla/firefox/*.default-release; do
+    stow --restow --target="$dir" firefox
+done
+
 
 # symlink some other stuff that is "naked" in home dir
 stow --restow --target="$HOME" home

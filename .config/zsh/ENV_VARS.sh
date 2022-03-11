c89ade18d623afaef4e290fb56033e7b3d4db2d9
Felix <felix.ulmanu@gmail.com>
2022-03-10 20:19:42 -0500

    git setting to fix error

--- .config/zsh/ENV_VARS.sh
+++ .config/zsh/ENV_VARS.sh
@@ -58,3 +58,6 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
+
+# git config
+git config --global core.editor "nvim"

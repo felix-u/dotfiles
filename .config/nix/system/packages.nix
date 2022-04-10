9d1c0418e4c5b3368e7a337282c0f91ddffc153f
Felix <nope@nope.com>
2022-04-09 22:07:35 -0400

    Fix clangd

--- .config/nix/system/packages.nix
+++ .config/nix/system/packages.nix
@@ -38,9 +38,10 @@
         foot gh git neofetch starship stow wget
 
         # DEV
-        android-tools gcc gnumake go unstable.godot home-manager libresprite
-        python39Packages.pip python3Full unstable.deadnix unstable.statix
-        yarn
+        android-tools cmake clang-tools gcc gnumake go unstable.godot
+        home-manager
+        libresprite python39Packages.pip python3Full
+        unstable.clang unstable.deadnix unstable.statix yarn
 
         # MATHS
         bc gnuplot libqalculate maxima qalculate-gtk wxmaxima

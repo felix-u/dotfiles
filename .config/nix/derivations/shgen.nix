dd4a0297ed915be95e727a56c245761745891c83
felix-u <29074327+felix-u@users.noreply.github.com>
2022-05-24 12:08:01 -0400

    no sha256 required in derivation if using builtins

--- .config/nix/derivations/shgen.nix
+++ .config/nix/derivations/shgen.nix
@@ -1,9 +1,8 @@
   let
     pkgs = import <nixpkgs> { };
 
-    shgen-bin = pkgs.fetchurl {
+    shgen-bin = builtins.fetchurl {
       url = "https://github.com/felix-u/shgen/archive/refs/heads/master.zip";
-      sha256 = "716c533da9b34d0484ef15e4010340cc42538255f760d7e8bc1dcb9afd23a6da";
     };
   in
     pkgs.runCommand "shgen" {}

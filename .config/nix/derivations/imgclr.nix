d0513c3cd61cdb00b4a9ad6e5c01d5664cf6e7ee
felix-u <29074327+felix-u@users.noreply.github.com>
2022-06-06 11:45:11 -0400

    Update imgclr derivation

--- .config/nix/derivations/imgclr.nix
+++ .config/nix/derivations/imgclr.nix
@@ -4,12 +4,12 @@ in
 {rustPlatform}:
 rustPlatform.buildRustPackage {
     pname = "imgclr";
-    version = "0.pre";
+    version = "0.1";
     src = pkgs.fetchFromGitHub {
         owner = "felix-u";
         repo = "imgclr";
-        rev = "9830c55cbb9154734c99aad70127319c51e2cfe9";
-        sha256 = "sha256-7L2RkqJt7BPQz99jJ3PpOkifKO4KmVK34ljMTo8D7IA=";
+        rev = "0a261c64b4319567f13dde8e3b827ae19ee49334";
+        sha256 = "sha256-01Fe7O6BIHiUNtCrG2gfsLMRbq+TzSj/ywRf7JBUwKY=";
     };
-    cargoSha256 = "sha256-Jh/0JyccqslFvGuxKru3V4ZwCC2V547Pk+av3FIr8V0=";
+    cargoSha256 = "sha256-YkQSfiWVYcbKmhCmMdo9/g6LppGnztP9sXr4LgJVHm0=";
 }

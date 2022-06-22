df738b04b5c2356966eccd7e19e77b016b81e9ba
felix-u <29074327+felix-u@users.noreply.github.com>
2022-06-22 17:54:19 -0400

    Fix fish config

--- .config/fish/functions/funcs.fish
+++ .config/fish/functions/funcs.fish
@@ -1,7 +1,7 @@
 # wayland colour picker
 function clrpick
     while true;
-	grim -g "$(slurp -b "00000000" -p)" - -t png -o | \
+	grim -g (slurp -b "00000000" -p) - -t png -o | \
 	    convert png:- -format '%[pixel:s]\n' info:- | \
 	    awk -F '[(,)]' '{printf("#%02x%02x%02x\n",$2,$3,$4)}' | \
 	    pastel format hex

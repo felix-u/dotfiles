9c3d05123c6819b4cb3fb85895cebac0dfad69fe
felix-u <29074327+felix-u@users.noreply.github.com>
2022-07-10 23:49:08 -0400

    Update sha256 for theme.sh derivation

--- .config/nix/derivations/themesh.nix
+++ .config/nix/derivations/themesh.nix
@@ -1,9 +1,9 @@
   let
     pkgs = import <nixpkgs> { };
 
-    themesh-bin = pkgs.fetchurl {
+    themesh-bin = builtins.fetchurl {
       url = "https://git.io/JM70M";
-      sha256 = "25cae4e35944c03b04301c6e75dfdb95077a0c10bc80970891578af496f68644";
+      # sha256 = "25cae4e35944c03b04301c6e75dfdb95077a0c10bc80970891578af496f68644";
     };
   in
   pkgs.runCommand "themesh" {}

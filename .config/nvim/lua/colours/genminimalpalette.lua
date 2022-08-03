cf8ef7e3bd4e2d8963756d150d5c394d40e7a3ca
felix-u <29074327+felix-u@users.noreply.github.com>
2022-08-02 20:37:53 -0400

    nvim: minimalpalette file (may delete)

---
+++ .config/nvim/lua/colours/genminimalpalette.lua
@@ -0,0 +1,33 @@
+local palette = {
+    background  = {"$(wq background)", 0,  "background"},
+    foreground  = {"$(wq foreground)", 16, "foreground"},
+    black00     = {"$(wq color0)",  1,  "black00"},
+    red01       = {"$(wq color15)",  2,  "red01"},
+    green02     = {"$(wq color2)",  3,  "green02"},
+    yellow03    = {"$(wq color15)",  4,  "yellow03"},
+    blue04      = {"$(wq color15)",  5,  "blue04"},
+    magenta05   = {"$(wq color15)",  6,  "magenta05"},
+    cyan06      = {"$(wq color15)",  7,  "cyan06"},
+    grey07      = {"$(wq color7)",  8,  "grey07"},
+    black08     = {"$(wq color8)",  9,  "black08"},
+    red09       = {"$(wq color15)",  10, "red09"},
+    green10     = {"$(wq color10)", 11, "green10"},
+    yellow11    = {"$(wq color15)", 12, "yellow11"},
+    blue12      = {"$(wq color15)", 13, "blue12"},
+    magenta13   = {"$(wq color15)", 14, "magenta13"},
+    cyan14      = {"$(wq color15)", 15, "cyan14"},
+    white15     = {"$(wq color15)", 16, "white15"},
+
+    orange = {
+        "$(wq color15)",
+        3,
+        "orange",
+    },
+    pink = {
+        "$(wq color7)",
+        1,
+        "pink",
+    },
+}
+
+return palette

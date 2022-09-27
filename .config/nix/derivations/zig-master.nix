82a0a8a0c4f1f99e04d34bf82cf6fed8d9a88575
felix-u <29074327+felix-u@users.noreply.github.com>
2022-09-27 17:59:18 -0400

    Update zig

--- .config/nix/derivations/zig-master.nix
+++ .config/nix/derivations/zig-master.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> { };
 
   zig-bin = builtins.fetchurl {
-    url = "https://ziglang.org/builds/zig-linux-x86_64-0.10.0-dev.3871+b7d5582de.tar.xz";
+    url = "https://ziglang.org/builds/zig-linux-x86_64-0.10.0-dev.4187+e915b905e.tar.xz";
   };
 in
 pkgs.runCommand "zig" {}
@@ -10,6 +10,6 @@ pkgs.runCommand "zig" {}
       #!${pkgs.stdenv.shell}
       ${pkgs.gnutar}/bin/tar xf ${zig-bin}
       mkdir -p $out/bin
-      cp -r zig-linux-x86_64-0.10.0-dev.3871+b7d5582de/*  $out/bin/
+      cp -r zig-linux-x86_64-0.10.0-dev.4187+e915b905e/*  $out/bin/
       chmod +x $out/bin/zig
     ''

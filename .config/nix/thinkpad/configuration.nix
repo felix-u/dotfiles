c644201e92e8387fb48d0200b7ce7b5fbacc7669
felix-u <29074327+felix-u@users.noreply.github.com>
2022-12-01 17:56:57 -0500

    More slight tweaks

--- .config/nix/thinkpad/configuration.nix
+++ .config/nix/thinkpad/configuration.nix
@@ -71,7 +71,7 @@
     services.tlp = {
         enable = true;
         settings = {
-            START_CHARGE_THRESH_BAT0 = 70;
+            # START_CHARGE_THRESH_BAT0 = 70;
             STOP_CHARGE_THRESH_BAT0 = 75;
         };
     };

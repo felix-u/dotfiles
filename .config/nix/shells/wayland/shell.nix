63d19580038f5e886d21c72b539877f6309dce9f
felix-u <29074327+felix-u@users.noreply.github.com>
2023-01-19 21:49:10 -0500

    Update zig

---
+++ .config/nix/shells/wayland/shell.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixos-unstable> {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cairo json_c meson pcre2 pkg-config wayland wayland-protocols wlroots_0_16
+    libxkbcommon libevdev pixman
+  ];
+}

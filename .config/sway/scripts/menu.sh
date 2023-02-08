1beaf409bfd7ca537036c95f4d552271272514de
felix-u <29074327+felix-u@users.noreply.github.com>
2023-02-07 22:30:01 -0500

    tweak tofi colours

--- .config/sway/scripts/menu.sh
+++ .config/sway/scripts/menu.sh
@@ -7,10 +7,10 @@ wq () {
 PADDINGV="4%"
 PADDINGH="2%"
 tofi-drun --font "$(wq fontmono)" --width 36% --height 50% --hint-font false \
-    --background-color "$(wq foreground)" --text-color "$(wq background)" \
+    --background-color "$(wq background)" --text-color "$(wq color15)" \
     --font-size 20 --prompt-text " λ " --outline-width 0 --border-width 4 \
-    --border-color "$(wq color8)" --selection-color "$(wq foreground)" \
-    --selection-background "$(wq background)" --selection-padding 16 \
+    --border-color "$(wq color8)" --selection-color "$(wq background)" \
+    --selection-background "$(wq color5)" --selection-padding 16 \
     --result-spacing 9 --padding-top $PADDINGV --padding-bottom $PADDINGV \
     --padding-left $PADDINGH --padding-right $PADDINGH --fuzzy-match true \
     --drun-launch true

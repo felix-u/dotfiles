c584c00972d344162572ac8a8647b32d0ab57d26
felix-u <29074327+felix-u@users.noreply.github.com>
2023-03-03 21:20:46 -0500

    print volume level in mute message

--- .config/sway/scripts/notify-mute.sh
+++ .config/sway/scripts/notify-mute.sh
@@ -8,7 +8,7 @@ dunstctl close-all
 MUTE=$(pulsemixer --get-mute)
 VOLUME=$(pulsemixer --get-volume | cut -f 1 -d " ")
 if [ "$MUTE" = 0 ]; then
-    notify-send -h int:value:"$VOLUME" -h "string:hlcolor:$(wq color4)" "Unmute"
+    notify-send -h int:value:"$VOLUME" -h "string:hlcolor:$(wq color4)" "Unmute (${VOLUME}%)"
 else
-    notify-send -h int:value:"$VOLUME" -h "string:hlcolor:$(wq color1)" "Mute"
+    notify-send -h int:value:"$VOLUME" -h "string:hlcolor:$(wq color1)" "Mute (${VOLUME}%)"
 fi

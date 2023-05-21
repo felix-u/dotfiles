2972ffbf254058ba3aa98fe1382922140935ce58
felix-u <29074327+felix-u@users.noreply.github.com>
2023-05-20 23:44:33 -0400

    nvim: commented lsp-zero config

--- .config/nix/system/users.nix
+++ .config/nix/system/users.nix
@@ -100,6 +100,7 @@ in {
                 goyo-vim
                 harpoon
                 leap-nvim
+                # pkgs-unstable.vimPlugins.lsp-zero-nvim
                 plenary-nvim
                 targets-vim
                 vim-commentary
@@ -322,6 +323,60 @@ in {
               nnoremap <leader>fm :Maps<CR>
               nnoremap <leader>fh :Helptags<CR>
               nnoremap <leader>ft :Filetypes<CR>
+
+              " lua << EOF
+              "   local lsp = require("lsp-zero")
+              "   lsp.preset(recommended)
+              "   lsp.ensure_installed({ })
+              "   lsp.nvim_workspace()
+              "   local cmp = require('cmp')
+              "   local cmp_select = {behavior = cmp.SelectBehavior.Select}
+              "   local cmp_mappings = lsp.defaults.cmp_mappings({
+              "     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+              "     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+              "     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+              "     ["<C-k>"] = cmp.mapping.complete(),
+              "   })
+              "   cmp_mappings['<Tab>'] = nil
+              "   cmp_mappings['<S-Tab>'] = nil
+              "   lsp.setup_nvim_cmp({
+              "     mapping = cmp_mappings
+              "   })
+              "   lsp.set_preferences({
+              "       suggest_lsp_servers = true,
+              "       sign_icons = {
+              "           error = 'E',
+              "           warn = 'W',
+              "           hint = 'H',
+              "           info = 'I'
+              "       }
+              "   })
+              "   lsp.on_attach(function(client, bufnr)
+              "     local opts = {buffer = bufnr, remap = false}
+              "     vim.keymap.set("n", "<leader>ld", function() vim.lsp.buf.definition() end, opts)
+              "     vim.keymap.set("n", "<leader>lk", function() vim.lsp.buf.hover() end, opts)
+              "     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+              "     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+              "     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+              "     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+              "     vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+              "     vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts)
+              "     vim.keymap.set("n", "<leader>ln", function() vim.lsp.buf.rename() end, opts)
+              "     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+              "   end)
+              "   lsp.setup()
+              "   vim.diagnostic.config({
+              "       virtual_text = true
+              "   })
+              "   local notify = vim.notify
+              "   vim.notify = function(msg, ...)
+              "       if msg:match("warning: multiple different client offset_encodings") then
+              "           return
+              "       end
+
+              "       notify(msg, ...)
+              "   end
+              " EOF
 
             '';
         };

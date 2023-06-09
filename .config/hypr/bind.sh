e678ac2cf7368bda1f5c4f6d840061f36d7406db
felix-u <29074327+felix-u@users.noreply.github.com>
2023-06-09 00:00:40 -0400

    hyprland config work

---
+++ .config/hypr/bind.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env sh
+
+alias bind="hyprctl keyword bind"
+
+wq () {
+    grep "$1:" "$XRESOURCES" | awk '{print $2}'
+}
+wqs () {
+    wq "$1" | tr -d \#
+}
+
+# Save theme colours.
+WFG="$(wq foreground)"
+WBG="$(wq background)"
+W00="$(wq color0)"
+W01="$(wq color1)"
+W02="$(wq color2)"
+W03="$(wq color3)"
+W04="$(wq color4)"
+W05="$(wq color5)"
+W06="$(wq color6)"
+W07="$(wq color7)"
+W08="$(wq color8)"
+W15="$(wq color15)"
+WSFG="$(wqs foreground)"
+WSBG="$(wqs background)"
+WS00="$(wqs color0)"
+WS01="$(wqs color1)"
+WS02="$(wqs color2)"
+WS03="$(wqs color3)"
+WS04="$(wqs color4)"
+WS05="$(wqs color5)"
+WS06="$(wqs color6)"
+WS07="$(wqs color7)"
+WS08="$(wqs color8)"
+WS15="$(wqs color15)"
+
+# Layout-independent bindings
+
+# Apps and scripts
+browser="firefox"
+files="io.elementary.files"
+term="foot"
+bind "SUPER, B, exec, $browser" &
+bind "SUPER, A, exec, $files" &
+bind "SUPER, Return, exec, $term" &
+bind "SUPER, D, exec, $XDG_CONFIG_HOME/sway/scripts/menu.sh" &
+
+# Window management
+bind "SUPER, W, killactive," &
+bind "SUPER ALT, Q, exit" &
+bind "SUPER, S, togglefloating," &
+bind "SUPER, T, togglesplit," &
+bind "SUPER, F, fullscreen, 0" &
+bind "SUPER ALT, F, fullscreen, 1" &
+bind "SUPER SHIFT, F, fakefullscreen," &
+bind "SUPER, Q, centerwindow," &
+hyprctl keyword bindm "SUPER,mouse:272, movewindow" &
+hyprctl keyword bindm "SUPER,mouse:273, resizewindow" &
+
+# Screenshots
+slurp_cmd="slurp -d -b '${WS07}40' -c '${WS07}' -w 3"
+pic_dir="$HOME/Pictures/screenshots/"
+hyprctl keyword bind "SUPER SHIFT, D, exec, $slurp_cmd | grim -g - $pic_dir/\$(date +%Y-%m-%d-%H:%M:%S).png" &
+hyprctl keyword bind "SUPER SHIFT, S, exec, $slurp_cmd | grim -g - /tmp/screenshot.png && cat /tmp/screenshot.png | \
+    wl-copy -t image/png" &
+hyprctl keyword bind "SUPER ALT, D, exec, grim $pic_dir/\$(date +%Y-%m-%d-%H:%M:%S).png" &
+hyprctl keyword bind "SUPER ALT, S, exec, grim /tmp/screenshot.png && cat /tmp/screenshot.png | \
+    wl-copy -t image/png" &
+
+# Notifications
+hyprctl keyword bind "SUPER, C, exec, dunstctl close" &
+hyprctl keyword bind "SUPER SHIFT, C, exec, dunstctl close-all" &
+hyprctl keyword bind "SUPER ALT, C, exec, dunstctl history-pop" &
+
+# Layout-dependent bindings
+
+if [ "$1" = "qwerty" ]; then
+    left="H"
+    down="J"
+    up="K"
+    right="L"
+    return="semicolon"
+elif [ "$1" = "colemak-dh" ]; then
+    left="M"
+    down="N"
+    up="E"
+    right="I"
+    return="O"
+fi
+
+# Window management
+
+for i in $(seq 1 9); do
+    bind "SUPER, $i, workspace, $i" &
+    bind "SUPER SHIFT, $i, movetoworkspace, $i" &
+done
+
+bind "SUPER, $left, movefocus, l" &
+bind "SUPER, $down, movefocus, d" &
+bind "SUPER, $up, movefocus, u" &
+bind "SUPER, $right, movefocus, r" &
+
+bind "SUPER SHIFT, $left, swapwindow, l" &
+bind "SUPER SHIFT, $down, swapwindow, d" &
+bind "SUPER SHIFT, $up, swapwindow, u" &
+bind "SUPER SHIFT, $right, swapwindow, r" &
+
+resize_amount=100
+bind "SUPER ALT, $left, resizeactive, -$resize_amount 0" &
+bind "SUPER ALT, $down, resizeactive, 0 $resize_amount" &
+bind "SUPER ALT, $up, resizeactive, 0 -$resize_amount" &
+bind "SUPER ALT, $right, resizeactive, $resize_amount 0" &
--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -13,6 +13,8 @@ wqs () {
 }
 
 # Wallpaper
+hyprctl keyword misc:disable_hyprland_logo true &
+hyprctl keyword misc:disable_splash_rendering true &
 "$XDG_CONFIG_HOME"/sway/scripts/randwall.sh "$DOTFILES"/Pictures/cafe-walls &
 
 # Warm the display based on time.
@@ -46,15 +48,16 @@ WS08="$(wqs color8)"
 WS15="$(wqs color15)"
 
 # Apps
-term="foot"
+browser="firefox"
 files="io.elementary.files"
+term="foot"
 
 # Bar
 # TODO
 
 # Thinkpad
 if [ "$(cat /proc/sys/kernel/hostname)" = "thonkpad" ]; then
-    dpi=2
+    dpi="2"
 
     # Brightness control
     hyprctl keyword bind ", XF86MonBrightnessUp, exec, brightnessctl set +3%" &
@@ -65,4 +68,49 @@ if [ "$(cat /proc/sys/kernel/hostname)" = "thonkpad" ]; then
 
     # Display output
     hyprctl keyword monitor ", 3840x2400@60, 0x0, $dpi" &
+
+    # Gestures
+    hyprctl keyword gestures:workspace_swipe on &
+
+    # Keybindings
+    "$XDG_CONFIG_HOME"/hypr/bind.sh "qwerty" &
+elif [ "$(cat /proc/sys/kernel/hostname)" = "nixbtw" ]; then
+    dpi="1.3"
+    
+    # Display output
+    hyprctl keyword monitor ", 3840x2160@60, 0x0, $dpi" &
+
+    # Keybindings
+    "$XDG_CONFIG_HOME"/hypr/bind.sh "colemak-dh" &
 fi
+
+# Notifications
+pkill dunst; dunst &
+
+# Gaps
+gaps=9
+hyprctl keyword general:gaps_in "$gaps" &
+hyprctl keyword general:gaps_out "$((gaps * 2))" &
+
+# Window borders
+hyprctl keyword general:border_size 3 &
+hyprctl keyword general:col.active_border "rgb($WSFG)" &
+hyprctl keyword general:col.inactive_border "rgb($WS06)" &
+hyprctl keyword decoration:rounding 0 &
+hyprctl keyword decoration:drop_shadow no &
+
+# Blur
+hyprctl keyword decoration:blur no &
+
+# Animations
+hyprctl keyword animations:enabled no &
+
+# Layout behaviour
+hyprctl keyword general:layout dwindle &
+hyprctl keyword dwindle:force_split 2 &
+hyprctl keyword dwindle:pseudotile no &
+hyprctl keyword dwindle:preserve_split yes &
+hyprctl keyword misc:enable_swallow true &
+
+# Polkit
+/run/current-system/sw/libexec/polkit-gnome-authentication-agent-1 &

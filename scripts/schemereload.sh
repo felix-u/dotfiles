e527b9d3d3b4ae4e3d4a7ed3809367c5974c7d76
felix-u <29074327+felix-u@users.noreply.github.com>
2023-08-07 16:46:55 -0400

    update schemereload script

--- scripts/schemereload.sh
+++ scripts/schemereload.sh
@@ -2,23 +2,11 @@
 
 NIXDIR="$XDG_CONFIG_HOME/nix/config"
 
-# fontconfig
-shgen "$DOTSCONF"/fontconfig/genfonts.conf "$DOTSCONF"/fontconfig/fonts.conf &
-
-# foot
-shgen "$DOTSCONF"/foot/gencolours.ini "$DOTSCONF"/foot/colours.ini &
-
 # darktable
 shgen "$XDG_CONFIG_HOME"/darktable/genuser.css "$XDG_CONFIG_HOME"/darktable/user.css &
-
-# firefox
-shgen "$NIXDIR"/firefox/genuserChrome.css "$NIXDIR"/firefox/userChrome.css &
 
 # imv
 shgen "$XDG_CONFIG_HOME"/imv/genconfig "$XDG_CONFIG_HOME"/imv/config &
 
 # waybar
 shgen "$DOTSCONF"/waybar/genstyle.css "$DOTSCONF"/waybar/style.css &
-
-# zathura
-shgen "$DOTSCONF"/zathura/genzathurarc "$DOTSCONF"/zathura/zathurarc &

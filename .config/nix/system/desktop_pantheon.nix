b9b6a58de0fa00fce189c585d2278b1f107b7fc4
felix-u <29074327+felix-u@users.noreply.github.com>
2023-10-10 22:44:36 -0400

    another failed attempt to get pantheon working

--- .config/nix/system/desktop_pantheon.nix
+++ .config/nix/system/desktop_pantheon.nix
@@ -4,32 +4,6 @@ let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
 {
-
-  environment.pathsToLink = [ "/libexec" ]; # for polkit
-  # qt.platformTheme = "qt5ct";
-
-  # pipewire
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # enable CUPS for printing
-  services.printing.enable = true;
-
-  services.flatpak.enable = true;
-  environment.sessionVariables = { GTK_USE_PORTAL = "1"; };
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal
-      xdg-desktop-portal-gnome
-    ];
-  };
-
   # bluetooth
   hardware.bluetooth = {
     enable = true;
@@ -39,13 +13,6 @@ in
   services.blueman.enable = true;
 
   hardware.opengl.enable = true;
-
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     gnome = unstable.gnome;
-  #     gnomeExtensions = unstable.gnomeExtensions;
-  #   })
-  # ];
 
   services.xserver = {
     enable = true;

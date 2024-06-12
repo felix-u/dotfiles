df8285f843ae9074790f1053390863347a753bdf
felix-u <29074327+felix-u@users.noreply.github.com>
2024-06-12 14:24:05 -0400

    les settings

--- link/home/.config/dwm/autostart.sh
+++ link/home/.config/dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 xset r rate 200 50
-
 hsetroot -solid "#$CLR_08"
+xsetroot -cursor_name left_ptr
 
 pkill picom; picom -b --vsync
--- link/home/.config/dwm/autostart_blocking.sh
+++ link/home/.config/dwm/autostart_blocking.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
 xrdb "$HOME"/.Xresources
+
+export GDK_SCALE=2
+export GDK_DPI_SCALE=2
+export QT_ENABLE_HIGHDPI_SCALING=1
+export QT_SCREEN_SCALE_FACTOR=2

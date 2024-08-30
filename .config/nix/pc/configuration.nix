111b5f2902e7454cfdc5681d3f69a7278260e411
felix-u <29074327+felix-u@users.noreply.github.com>
2024-08-30 17:45:14 -0400

    idk

--- .config/nix/pc/configuration.nix
+++ .config/nix/pc/configuration.nix
@@ -56,22 +56,22 @@
     ];
   };
 
-  nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
-    (final: prev: {
-      gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
-        mutter = gnomePrev.mutter.overrideAttrs (old: {
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.gnome.org";
-            owner = "vanvugt";
-            repo = "mutter";
-            rev = "triple-buffering-v4-46";
-            hash = "sha256-nz1Enw1NjxLEF3JUG0qknJgf4328W/VvdMjJmoOEMYs=";
-          };
-        });
-      });
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   # GNOME 46: triple-buffering-v4-46
+  #   (final: prev: {
+  #     gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+  #       mutter = gnomePrev.mutter.overrideAttrs (old: {
+  #         src = pkgs.fetchFromGitLab {
+  #           domain = "gitlab.gnome.org";
+  #           owner = "vanvugt";
+  #           repo = "mutter";
+  #           rev = "triple-buffering-v4-46";
+  #           hash = "sha256-nz1Enw1NjxLEF3JUG0qknJgf4328W/VvdMjJmoOEMYs=";
+  #         };
+  #       });
+  #     });
+  #   })
+  # ];
 
   system.stateVersion = "23.05";
 }
